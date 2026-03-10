4a1bf2598a69b3b8b1eb9bdb9ac880cffbe3b7f2
Huntero08 <lanavicentin0106@gmail.com>
2026-03-09 21:05:58 -0300

    criação da atividade2

---
+++ js_base/variaveisTipos.js
@@ -0,0 +1,26 @@
+let nome = `lana`;
+const sobrenome = `landim`;
+let templateSring = `o meu nome ${nome} ${sobrenome}`;
+console.log(templateSring);
+let x1 = 10;
+let x2 = -5;
+let A = 2;
+let B = 5.56;
+let c = -3.14;
+const PI = 3.14;
+let potencia = A ** c;
+console.log (typeof A);
+console.log (typeof c);
+console.log(`${A}^${C}=${potencia} `);
+
+let verdadeiro = true;
+let falso = false;
+console.log(typeof verdadeiro);
+
+let E = verdadeiro && falso;
+let ou = verdadeiro || falso;
+console.log(E);
+console.log(ou);
+
+let maior_que = B > A;
+console.log (`${B}>${A}=${maior_que}`);
